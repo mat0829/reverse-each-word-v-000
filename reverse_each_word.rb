f7c636ebce421cdb6b44c536581533900681def5
David Anderson <matandersonlv@hotmail.com>
2018-10-15 20:09:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   my_statements = []
   sentence.each do |sentence|
-    my_statements << "#{sentence.reverse}"
+    my_statements << puts "#{sentence.reverse}"
   end
   my_statements
 end
