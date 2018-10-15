5f4f32abf137233d1a1ced42a818881ae0c7618f
David Anderson <matandersonlv@hotmail.com>
2018-10-15 20:11:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   my_statements = []
   sentence.each do |sentence|
-    my_statements << puts "#{sentence}".reverse
+    my_statements << puts "#{sentence}"
   end
   my_statements
 end
